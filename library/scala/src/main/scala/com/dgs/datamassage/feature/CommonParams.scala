02a1a5ebe292ce4e003cbb9a534ab80ccfe1f726
Calin Cocan <calin.cocan@datagridsoftware.com>
2021-03-04 09:46:00 +0200

    feat: fix basic functionality for MissingInfuser

--- library/scala/src/main/scala/com/dgs/datamassage/feature/CommonParams.scala
+++ library/scala/src/main/scala/com/dgs/datamassage/feature/CommonParams.scala
@@ -1,13 +1,17 @@
 package com.dgs.datamassage.feature
 
 import org.apache.spark.ml.param.{Param, ParamValidators, Params, StringArrayParam}
+import org.json4s.{DefaultFormats, JDouble, JLong}
+import org.json4s.JsonAST.{JBool, JObject}
+import org.json4s.jackson.JsonMethods.{compact, parse, render}
 
-import java.util.Date
+import java.sql.Timestamp
 
-trait HasCatNumericalInputCols extends Params {
+trait HasCategoricalNumericalInputCols extends Params {
 
   /**
    * Param for categorical numeric input column names.
+   *
    * @group param
    */
   final val catNumericalInputCols: StringArrayParam = new StringArrayParam(this, "catNumericalInputCols", "categorical numeric input column names")
@@ -20,12 +24,22 @@ trait NullStringParam extends Params {
 
   /**
    * Param for default value used in case of null value encounter on string fields.
+   *
    * @group param
    */
   val nullStringParam: Param[String] = new Param[String](this, "nullStringParam", "default value for null value encounter on string field")
 
+
+  /**
+   *
+   * @param value String
+   * @return this.type
+   */
+  def setNullStringParam(value: String): this.type = set(nullStringParam, value)
+
   /** @group getParam */
   final def getNullStringParam: String = $(nullStringParam)
+
 
   setDefault(nullStringParam, "__NULL__")
 }
@@ -34,9 +48,28 @@ trait NullBooleanParam extends Params {
 
   /**
    * Param for default value used in case of null value encounter on boolean fields.
+   *
    * @group param
    */
-  val nullBooleanParam: Param[Boolean] = new Param[Boolean](this, "nullBooleanParam", "default value for null value encounter on boolean fields")
+  val nullBooleanParam: Param[Boolean] = new Param[Boolean](this, "nullBooleanParam", "default value for null value encounter on boolean fields") {
+    /** Encodes a param value into JSON, which can be decoded by `jsonDecode()`. */
+    override def jsonEncode(value: Boolean): String = compact(render(JBool(value)))
+
+    /** Decodes a param value from JSON. */
+    override def jsonDecode(json: String): Boolean = {
+      implicit val formats = DefaultFormats
+      val jObject = parse(json).extract[JObject]
+      val boolParamValue = (jObject \ "nullBooleanParam").extract[Boolean]
+      boolParamValue
+    }
+  }
+
+  /**
+   *
+   * @param value Boolean
+   * @return this.type
+   */
+  def setNullBooleanParam(value: Boolean): this.type = set(nullBooleanParam, value)
 
   /** @group getParam */
   final def getNullBooleanParam: Boolean = $(nullBooleanParam)
@@ -49,25 +82,65 @@ trait NullNumericParam extends Params {
 
   /**
    * Param for default value used in case of null value encounter on numeric fields.
+   *
    * @group param
    */
-  val nullNumericParam: Param[Double] = new Param[Double](this, "nullNumericParam", "default value for null value encounter on numeric fields")
+  val nullNumericParam: Param[Double] = new Param[Double](this, "nullNumericParam", "default value for null value encounter on numeric fields") {
+    /** Encodes a param value into JSON, which can be decoded by `jsonDecode()`. */
+    override def jsonEncode(value: Double): String = compact(render(JDouble(value)))
+
+    /** Decodes a param value from JSON. */
+    override def jsonDecode(json: String): Double = {
+      implicit val formats = DefaultFormats
+      val jObject = parse(json).extract[JObject]
+      val doubleParamValue = (jObject \ "nullNumericParam").extract[Double]
+      doubleParamValue
+    }
+  }
 
   /** @group getParam */
   final def getNullNumericParam: Double = $(nullNumericParam)
 
+  /**
+   *
+   * @param value Double
+   * @return this.type
+   */
+  def setNullNumericParam(value: Double): this.type = set(nullNumericParam, value)
+
   setDefault(nullNumericParam, 0.0d)
 }
 
-trait NullDateParam extends Params {
+trait NullTimestampParam extends Params {
 
   /**
    * Param for default value used in case of null value encounter on date / time /timestamp fields.
+   *
    * @group param
    */
-  val nullDateParam: Param[Date] = new Param[Date](this, "nullDateParam", "default value for null value encounter on date / time / timestamp fields")
+  val nullTimestampParam: Param[Timestamp] = new Param[Timestamp](this, "nullTimestampParam", "default value for null value encounter on date / time / timestamp fields") {
+    /** Encodes a param value into JSON, which can be decoded by `jsonDecode()`. */
+    override def jsonEncode(value: Timestamp): String = compact(render(JLong(value.getTime)))
+
+    /** Decodes a param value from JSON. */
+    override def jsonDecode(json: String): Timestamp = {
+      implicit val formats = DefaultFormats
+      val jObject = parse(json).extract[JObject]
+      val longParamValue = (jObject \ "nullTimestampParam").extract[Long]
+      new Timestamp(longParamValue)
+    }
+  }
+
+
+  /**
+   *
+   * @param value Double
+   * @return this.type
+   */
+  def setNullTimestampParam(value: Timestamp): this.type = set(nullTimestampParam, value)
 
   /** @group getParam */
-  final def getNullDateParam: Date = $(nullDateParam)
-  setDefault(nullDateParam, new Date(0))
+  final def getNullTimestampParam: Timestamp = $(nullTimestampParam)
+
+  setDefault(nullTimestampParam, new Timestamp(0))
 }
